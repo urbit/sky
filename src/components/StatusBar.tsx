ca170c925737c5b6f77e9e44bcfebf574757810b
bonbud-macryg <oarsman_phlox.0s@icloud.com>
2024-10-24 15:52:29 +0100

    Dummy status bar

---
+++ src/components/StatusBar.tsx
@@ -0,0 +1,33 @@
+import '@urbit/sigil-js'
+import bellIcon from '../assets/images/bell.png'
+import closeIcon from '../assets/images/close.png'
+
+const sigilConfig = {
+  size: '30px',
+  // TODO remove hard-coded ship
+  point: '~sampel-palnet',
+  // TODO get colors from tlon/landscape user preferences
+  foreground: '#FFF',
+  background: '#000',
+  detail: 'none',
+  space: 'default'
+}
+
+export default function StatusBar() {
+  return (
+    <div style={{ width: '100%', padding: '10px', display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
+      <div style={{ height: '30px', backgroundColor: 'lightgray', borderRadius: '3px', paddingLeft: '10px', paddingRight: '10px', display: 'flex', alignItems: 'center', justifyContent: 'space-between', width: '200px' }}>
+        <span>Workspace 1</span>
+        <div>
+          <img style={{ height: '10px', width: '10px' }} src={closeIcon} alt="Close space" />
+        </div>
+      </div>
+      <div style={{ height: '30px', display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+        <div style={{ width: '30px', backgroundColor: 'lightgray', borderRadius: '3px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+          <img src={bellIcon} alt="Open notifications" style={{ height: '20px' }} />
+        </div>
+        <urbit-sigil style={{ overflow: 'hidden', borderRadius: '3px', marginLeft: '5px' }} {...sigilConfig} />
+      </div>
+    </div>
+  );
+}
