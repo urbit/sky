bb971327ff83fcf39b1e627ddb7479126ba41528
bonbud-macryg <oarsman_phlox.0s@icloud.com>
2024-10-28 15:24:22 +0000

    Fix appearance of round corners

--- src/components/Window.tsx
+++ src/components/Window.tsx
@@ -15,14 +15,17 @@ export default function Window({ id, path }: WindowProps) {
     //  TODO move b1 br1 classes to a div inside the pane
     //  right now it looks as if corners are being cut off
     //  by the space between windows
-    <div className='b1 br1 fc ac jc' style={{ width: '100%', height: '100%' }} onClick={handleClick}>
+    <Allotment>
       <Allotment.Pane
         visible
         key={id}
-        preferredSize="100%"
       >
-        <p></p>
+        <div className='fc ac jc' style={{ width: '100%', height: '100%', padding: '5px' }} onClick={handleClick}>
+          <div className='fc ac jc b1 br1' style={{ width: '100%', height: '100%' }}>
+            <p>Window {id}</p>
+          </div>
+        </div>
       </Allotment.Pane>
-    </div>
+    </Allotment>
   )
 }
