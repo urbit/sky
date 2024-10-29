127695fff184785ffbc38fccace4147ecb7b2991
bonbud-macryg <101650933+bonbud-macryg@users.noreply.github.com>
2024-10-29 16:40:53 +0000

    Add rough navbar and iframes in windows (#13)

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,69 @@
+import { useState } from 'react'
+import { NavBarProps } from '../types/navbar.ts'
+
+export default function NavBar({ path }: NavBarProps) {
+  const [hovered, setHovered] = useState(false)
+  const [isEditing, setIsEditing] = useState(false)
+  const [inputValue, setInputValue] = useState(path)
+
+  function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setInputValue(e.target.value)
+  }
+
+  function handlePathClick() {
+    setIsEditing(true)
+  }
+
+  function handleInputBlur() {
+    setIsEditing(false)
+  }
+
+  function handleSubmit() {
+    // TODO change the path for this node in state
+    // TODO get() this path and render results in the iframe
+  }
+
+  return (
+    <div
+      className='fr ac jb b2'
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
+      style={{
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '30px',
+        paddingRight: '10px',
+        opacity: hovered ? 1.0 : 0.0,
+      }}
+    >
+      <div
+        className='fr ac'
+        style={{ width: '100%', height: '100%' }}
+        onClick={handlePathClick}
+      >
+        {isEditing ? (
+          <form style={{ width: '100%' }} onSubmit={handleSubmit}>
+            <input
+              type='text'
+              className='b3'
+              value={inputValue}
+              onChange={handleInputChange}
+              onBlur={handleInputBlur}
+              style={{ width: '100%' }}
+            />
+          </form>
+        ) : (
+          // TODO break up into breadcrumbs
+          <p>{path}</p>
+        )}
+      </div>
+      {/* TODO add real buttons */}
+      <div className='fr ac jb' style={{ width: '30px' }}>
+        <p>_</p>
+        <p>x</p>
+      </div>
+    </div>
+  )
+}
--- src/state/windowState.ts
+++ src/state/windowState.ts
@@ -3,6 +3,6 @@ import { WindowNode } from '../types/windows.ts'
 export default interface WindowState {
   windowTree: WindowNode
   addWindowNode: (parentId: number, path: string) => void
-  delWindowNode: (id: string) => void
+  delWindowNode: (id: number) => void
   clearWindows: () => void
 }
