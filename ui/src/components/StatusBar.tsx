2a32a12228205c66d363f0434b3eb84bb1d7d218
bonbud-macryg <101650933+bonbud-macryg@users.noreply.github.com>
2024-11-08 17:55:06 +0000

    Verify path input in navbar (#22)

--- ui/src/components/StatusBar.tsx
+++ ui/src/components/StatusBar.tsx
@@ -1,59 +1,59 @@
-import '@urbit/sigil-js'
-import bellIcon from '../assets/images/bell.png'
-import closeIcon from '../assets/images/close.png'
+import "@urbit/sigil-js";
+import bellIcon from "../assets/images/bell.png";
+import closeIcon from "../assets/images/close.png";
 
 const sigilConfig = {
   // TODO don't hard-code height all over this component
   // changing size in sigilConfig upsets layout
-  size: '30px',
+  size: "30px",
   // TODO remove hard-coded ship
-  point: '~sampel-palnet',
+  point: "~sampel-palnet",
   // TODO get colors from tlon/landscape user preferences
-  foreground: '#FFF',
-  background: '#000',
-  detail: 'none',
-  space: 'default'
-}
+  foreground: "#FFF",
+  background: "#000",
+  detail: "none",
+  space: "default",
+};
 
 export default function StatusBar() {
   return (
-    <div className="fr ac jb" style={{ height: '50px' }}>
+    <div className="fr ac jb" style={{ height: "50px" }}>
       <div
         className="br1 fr ac jb b1"
         style={{
-          height: '30px',
-          paddingLeft: '10px',
-          paddingRight: '10px',
-          width: '200px'
+          height: "30px",
+          paddingLeft: "10px",
+          paddingRight: "10px",
+          width: "200px",
         }}
       >
         <span>Workspace 1</span>
         <div>
           <img
-            style={{ height: '10px', width: '10px' }}
+            style={{ height: "10px", width: "10px" }}
             src={closeIcon}
             alt="Close space"
           />
         </div>
       </div>
-      <div className="fr ac jb" style={{ height: '30px' }}>
+      <div className="fr ac jb" style={{ height: "30px" }}>
         <div
           className="fr ac jc br1 b1"
-          style={{ width: '30px', height: '30px' }}
+          style={{ width: "30px", height: "30px" }}
         >
           <img
             src={bellIcon}
             alt="Open notifications"
-            style={{ height: '20px' }}
+            style={{ height: "20px" }}
           />
         </div>
         <div
           className="br1 scroll-hidden"
-          style={{ height: '30px', marginLeft: '5px' }}
+          style={{ height: "30px", marginLeft: "5px" }}
         >
           <urbit-sigil {...sigilConfig} />
         </div>
       </div>
     </div>
-  )
+  );
 }
--- ui/src/components/renderers/WebPage.tsx
+++ ui/src/components/renderers/WebPage.tsx
@@ -1,11 +1,11 @@
 export default function WebPage({ page }: { page: Response }): JSX.Element {
-  console.log('page: ', page)
+  console.log("page: ", page);
 
   return (
     <iframe
       //srcDoc={htmlContent || ''}
       src="https://urbit.org"
-      style={{ width: '100%', height: '100%', border: 'none' }}
+      style={{ width: "100%", height: "100%", border: "none" }}
     />
-  )
+  );
 }
