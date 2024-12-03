4a7d46c1bab94633900d9798073fd0e78814711f
bonbud-macryg <oarsman_phlox.0s@icloud.com>
2024-12-03 13:19:09 +0000

    FileMarkdown draft

--- ui/src/components/renderers/FileMarkdown.tsx
+++ ui/src/components/renderers/FileMarkdown.tsx
@@ -1,7 +1,13 @@
+import Editor from '@monaco-editor/react';
+
 interface FileMarkdownProps {
   md: string
 }
 
 export default function FileMarkdown({ md }: FileMarkdownProps): JSX.Element {
-  return <p>FileMarkdown component</p>
+  return (
+    <div className='hf wf' style={{ overflow: 'scroll' }}>
+      <Editor height="100%" defaultLanguage="markdown" defaultValue={md} />
+    </div>
+  )
 }
