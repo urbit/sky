7028eac4a70b6703723e0246dcee412f5ad1daae
bonbud-macryg <oarsman_phlox.0s@icloud.com>
2024-12-03 12:59:03 +0000

    TextMarkdown component

--- ui/src/components/renderers/TextMarkdown.tsx
+++ ui/src/components/renderers/TextMarkdown.tsx
@@ -1,6 +1,13 @@
+import Markdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
+
 interface TextMarkdownProps {
   md: string
 }
 export default function TextMarkdown({ md }: TextMarkdownProps) {
-  return <p>TextMarkdown component</p>
+  return (
+    <div className='p2' style={{ overflow: 'scroll' }}>
+      <Markdown remarkPlugins={[remarkGfm]}>{md}</Markdown>
+    </div>
+  )
 }
