acda3e7c7004d424365e2c384f7ebeac4948be75
bonbud-macryg <oarsman_phlox.0s@icloud.com>
2024-12-05 16:40:57 +0000

    Max-width to prose class

--- ui/src/components/renderers/TextMarkdown.tsx
+++ ui/src/components/renderers/TextMarkdown.tsx
@@ -6,7 +6,7 @@ interface TextMarkdownProps {
 }
 export default function TextMarkdown({ md }: TextMarkdownProps) {
   return (
-    <div className="p2 prose" style={{ overflow: 'scroll' }}>
+    <div className="p2 prose" style={{ overflowY: 'scroll', overflowX: 'hidden' }}>
       <Markdown remarkPlugins={[remarkGfm]}>{md}</Markdown>
     </div>
   )
