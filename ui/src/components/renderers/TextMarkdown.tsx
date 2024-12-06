f2e71b10ac03f19a8a40fe1a7e6b1dffcd6cb215
bonbud-macryg <oarsman_phlox.0s@icloud.com>
2024-12-06 11:30:28 +0000

    Support inline HTML in Markdown files

--- ui/src/components/renderers/TextMarkdown.tsx
+++ ui/src/components/renderers/TextMarkdown.tsx
@@ -1,5 +1,6 @@
 import Markdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
+import rehypeRaw from 'rehype-raw'
 
 interface TextMarkdownProps {
   md: string
@@ -8,7 +9,7 @@ export default function TextMarkdown({ md }: TextMarkdownProps) {
   return (
     <div className="hf wf fr as jc p2" style={{ overflowY: 'scroll', overflowX: 'hidden' }}>
       <div>
-        <Markdown className='prose' remarkPlugins={[remarkGfm]}>{md}</Markdown>
+        <Markdown className='prose' remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeRaw]}>{md}</Markdown>
       </div>
     </div>
   )
