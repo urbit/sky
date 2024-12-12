08d1fd23d37bd23389c6ee904d5f0dddadf32559
bonbud-macryg <oarsman_phlox.0s@icloud.com>
2024-12-12 14:02:17 +0000

    Ignore tsbuildinfo files

--- ui/src/components/renderers/TextHTML.tsx
+++ ui/src/components/renderers/TextHTML.tsx
@@ -1,33 +1,32 @@
-import React from 'react';
+import React from 'react'
 
 interface TextHTMLProps {
-  content: string;
-  isLocal: boolean;
+  content: string
+  isLocal: boolean
 }
 
 export default function TextHTML({ content, isLocal }: TextHTMLProps) {
   React.useEffect(() => {
     if (isLocal) {
-      const doc = document;
+      const doc = document
 
-      const hollowLink = doc.createElement('link');
-      hollowLink.rel = 'stylesheet';
-      hollowLink.href = 'hollow.css';
+      const hollowLink = doc.createElement('link')
+      hollowLink.rel = 'stylesheet'
+      hollowLink.href = 'hollow.css'
 
-      const spineLink = doc.createElement('link');
-      spineLink.rel = 'stylesheet';
-      spineLink.href = 'spine.css';
+      const spineLink = doc.createElement('link')
+      spineLink.rel = 'stylesheet'
+      spineLink.href = 'spine.css'
 
-      const featherLink = doc.createElement('link');
-      featherLink.rel = 'stylesheet';
-      featherLink.href = 'feather.css';
+      const featherLink = doc.createElement('link')
+      featherLink.rel = 'stylesheet'
+      featherLink.href = 'feather.css'
 
-      doc.head.appendChild(hollowLink);
-      doc.head.appendChild(spineLink);
-      doc.head.appendChild(featherLink);
+      doc.head.appendChild(hollowLink)
+      doc.head.appendChild(spineLink)
+      doc.head.appendChild(featherLink)
     }
-  }, []);
+  }, [])
 
-  return <div dangerouslySetInnerHTML={{ __html: content }} />;
+  return <div dangerouslySetInnerHTML={{ __html: content }} />
 }
-
