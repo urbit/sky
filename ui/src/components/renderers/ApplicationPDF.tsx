e1a053a0d81088fdda93585905fac87c8cca7bcf
SuperCoolYun <yunraccoon@gmail.com>
2024-12-16 14:21:37 +0100

    pdf application and file renderer

---
+++ ui/src/components/renderers/ApplicationPDF.tsx
@@ -0,0 +1,13 @@
+interface ApplicationPDFProps {
+    pdf: string
+  }
+
+export default function ApplicationPDF({ pdf }: ApplicationPDFProps): JSX.Element {
+    return(
+        <div className="hf wf fr as jc">
+        <iframe 
+        className="hf wf"
+        src={pdf}/>
+        </div>
+    )
+} 
