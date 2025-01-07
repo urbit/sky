03ddbbc0c661ed1ef79fbc9fb3026ab1a4a3b4e8
bonbud-macryg <oarsman_phlox.0s@icloud.com>
2025-01-07 13:58:51 +0000

    Remove comments from TextHTML

--- ui/src/components/renderers/TextHTML.tsx
+++ ui/src/components/renderers/TextHTML.tsx
@@ -4,34 +4,11 @@ interface TextHTMLProps {
 
 export default function TextHTML({ url }: TextHTMLProps) {
   if (url) {
-    //const parser = new DOMParser()
-    //const doc = parser.parseFromString(content, 'text/html')
-
-    //if (isLocal) {
-    //  // TODO link to ../stlye/... files rather than public
-    //  // vite won't follow links to style dir in dev mode
-    //  const hollowLink = doc.createElement('link')
-    //  hollowLink.rel = 'stylesheet'
-    //  hollowLink.href = 'hollow.css'
-    //
-    //  const spineLink = doc.createElement('link')
-    //  spineLink.rel = 'stylesheet'
-    //  spineLink.href = 'spine.css'
-    //
-    //  const featherLink = doc.createElement('link')
-    //  featherLink.rel = 'stylesheet'
-    //  featherLink.href = 'feather.css'
-    //
-    //  doc.head.appendChild(spineLink)
-    //  doc.head.appendChild(featherLink)
-    //}
-
     return (
       <div className="hf wf fr as jc">
         <iframe
           className="hf wf"
           src={url}
-          //srcDoc={new XMLSerializer().serializeToString(doc)}
           style={{ border: 'none', borderRadius: '2.5px' }}
           sandbox="allow-scripts"
         />
