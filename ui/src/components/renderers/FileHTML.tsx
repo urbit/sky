059e8faeaf80a956665e3df4e4b65b6f3f1d9d8d
bonbud-macryg <oarsman_phlox.0s@icloud.com>
2025-01-10 15:50:33 +0000

    pnpm build

--- ui/src/components/renderers/FileHTML.tsx
+++ ui/src/components/renderers/FileHTML.tsx
@@ -155,7 +155,9 @@ export default function FileHTML({ html }: FileHTMLProps): JSX.Element {
         setEditorContent(content)
         setIsEdited(true)
         const formData = new FormData()
-        const file = new File([content], `${pathArray.slice(-1)}.html`, { type: 'text/html' })
+        const file = new File([content], `${pathArray.slice(-1)}.html`, {
+          type: 'text/html',
+        })
         formData.append('file', file)
 
         try {
@@ -175,7 +177,9 @@ export default function FileHTML({ html }: FileHTMLProps): JSX.Element {
   const handlePublish = async () => {
     if (activeWindowPath) {
       const formData = new FormData()
-      const file = new File([editorContent], `${pathArray.slice(-1)}.html`, { type: 'text/html' })
+      const file = new File([editorContent], `${pathArray.slice(-1)}.html`, {
+        type: 'text/html',
+      })
       formData.append('file', file)
 
       try {
