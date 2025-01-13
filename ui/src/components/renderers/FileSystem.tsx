475d51dc0e76f0a55407491252cd721ab64a9d51
bonbud-macryg <oarsman_phlox.0s@icloud.com>
2025-01-13 11:43:18 +0000

    feat: allow XML file uploads

--- ui/src/components/renderers/FileSystem.tsx
+++ ui/src/components/renderers/FileSystem.tsx
@@ -154,7 +154,7 @@ export default function FileSystem({ id, path }: FileSystemProps): JSX.Element {
       <button onClick={handleUploadClick}>Upload file</button>
       <input
         type="file"
-        accept=".css, .html, .js, .json, .md, .pdf, .png, .txt"
+        accept=".css, .html, .js, .json, .md, .pdf, .png, .txt, .xml"
         style={{ display: 'none' }}
         onChange={uploadFiles}
       />
