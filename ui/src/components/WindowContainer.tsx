4701a87b5eee73565dcaf4b72234a5cd9deebec1
bonbud-macryg <101650933+bonbud-macryg@users.noreply.github.com>
2025-01-14 19:31:01 +0000

    Remove setMaxWindow from WindowProps (#80)

--- ui/src/components/WindowContainer.tsx
+++ ui/src/components/WindowContainer.tsx
@@ -20,7 +20,7 @@ export default function WindowContainer({
   handleDragStart,
   dragWindow,
 }: WindowContainerProps): JSX.Element {
-  const { delWindow, setMaxWindow } = useWindowStore()
+  const { delWindow } = useWindowStore()
   const lastChange = useRef<number[]>([])
 
   const childId = id * 2
@@ -86,7 +86,6 @@ export default function WindowContainer({
           handleDrop={handleDrop}
           handleDragStart={handleDragStart}
           dragWindow={dragWindow}
-          setMaxWindow={setMaxWindow}
         />
       ) : (
         // return a window container
