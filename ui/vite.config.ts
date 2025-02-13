7c174d2cdff6e6f1e97751d007acfe20d8e1acbd
bonbud-macryg <oarsman_phlox.0s@icloud.com>
2025-02-13 11:50:50 +0000

    Remove bad line from vite.config.js

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 import { urbitPlugin } from '@urbit/vite-plugin-urbit'
 
 export default defineConfig({
   plugins: [
-    react(),
     urbitPlugin({
       base: 'sky',
       target: 'http://localhost:8080',
