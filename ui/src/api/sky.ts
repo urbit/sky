e038deedad084b1e1348ab1cd038a034d3e0a577
bonbud-macryg <oarsman_phlox.0s@icloud.com>
2025-02-28 17:13:29 +0000

    Replace get()

--- ui/src/api/sky.ts
+++ ui/src/api/sky.ts
@@ -35,16 +35,11 @@ async function findPathUrl(path: string): Promise<string | void> {
 }
 
 // TODO rename to get() once finsihed
-async function newGet(path: string): Promise<Response | void> {
+async function get(path: string): Promise<Response | void> {
   const pathArray = path.split('/')
   const pathShip = pathArray[0].slice(1)
   const endpoint = pathArray.slice(1).join('/')
-  const url = await findPathUrl(`${pathArray[0]}/${endpoint}`)
-
-  if (!url) {
-    console.error(`No URL found for ${path}`)
-    return
-  }
+  const url = `${window.location.origin}/${endpoint}`
 
   // TODO handle relative get('/foo')
   // TODO handle relative get('foo') and get('~/foo')
@@ -100,97 +95,97 @@ async function newGet(path: string): Promise<Response | void> {
 }
 
 // NOTE provisional; will change with remote scry support
-async function get(path: string): Promise<Response | void> {
-  const pathArray = path.split('/')
-  const pathShip = pathArray[0].slice(1)
-  const endpoint = pathArray.slice(1).join('/')
-  const url = await findPathUrl(`${pathArray[0]}/${endpoint}`)
-
-  if (!url) {
-    console.error(`No URL found for ${path}`)
-
-    return new Response(`No URL found for ${path}`, {
-      status: 404,
-      headers: { 'Content-Type': 'text/plain' },
-    })
-  }
-
-  try {
-    const res = await fetch(url, {
-      method: 'GET',
-      credentials: 'include',
-    })
-
-    if (!res.ok) {
-      throw new Error(`Response not ok for ${url}`)
-    }
-
-    // TODO remove hard-coded URL
-    const redirectedToGrid =
-      endpoint !== '/apps/landscape' &&
-      res.url === `http://localhost:8080/apps/landscape/`
-
-    // NOTE handle Landscape redirect
-    // TODO change this behaviour in Landscape
-    if (redirectedToGrid) {
-      return new Response(`File not found for ${path}`, {
-        status: 404,
-        headers: {
-          'Content-Type': 'text/plain',
-          'X-Response-URL': url,
-        },
-      })
-    }
-
-    return res
-  } catch (err) {
-    console.error(`GET request failed at ${url}`, err)
-
-    if (pathShip === window.ship) {
-      const apiUrl = await findPathUrl(`${pathArray[0]}/api/${endpoint}`)
-
-      if (!apiUrl) {
-        console.error(`No URL found for ${path}`)
-
-        return new Response(`No URL found for ${path}`, {
-          status: 404,
-          headers: { 'Content-Type': 'text/plain' },
-        })
-      }
-
-      try {
-        const res = await fetch(apiUrl, {
-          method: 'GET',
-          credentials: 'include',
-        })
-
-        if (!res.ok) {
-          throw new Error(`Response not ok for ${apiUrl}`)
-        }
-
-        return res
-      } catch (err) {
-        console.error(`GET request failed at ${apiUrl}`, err)
-
-        return new Response(`File not found for ${path}`, {
-          status: 404,
-          headers: {
-            'Content-Type': 'text/plain',
-            'X-Response-URL': apiUrl,
-          },
-        })
-      }
-    }
-
-    return new Response(`File not found for ${path}`, {
-      status: 404,
-      headers: {
-        'Content-Type': 'text/plain',
-        'X-Response-URL': url,
-      },
-    })
-  }
-}
+//async function get(path: string): Promise<Response | void> {
+//  const pathArray = path.split('/')
+//  const pathShip = pathArray[0].slice(1)
+//  const endpoint = pathArray.slice(1).join('/')
+//  const url = await findPathUrl(`${pathArray[0]}/${endpoint}`)
+//
+//  if (!url) {
+//    console.error(`No URL found for ${path}`)
+//
+//    return new Response(`No URL found for ${path}`, {
+//      status: 404,
+//      headers: { 'Content-Type': 'text/plain' },
+//    })
+//  }
+//
+//  try {
+//    const res = await fetch(url, {
+//      method: 'GET',
+//      credentials: 'include',
+//    })
+//
+//    if (!res.ok) {
+//      throw new Error(`Response not ok for ${url}`)
+//    }
+//
+//    // TODO remove hard-coded URL
+//    const redirectedToGrid =
+//      endpoint !== '/apps/landscape' &&
+//      res.url === `http://localhost:8080/apps/landscape/`
+//
+//    // NOTE handle Landscape redirect
+//    // TODO change this behaviour in Landscape
+//    if (redirectedToGrid) {
+//      return new Response(`File not found for ${path}`, {
+//        status: 404,
+//        headers: {
+//          'Content-Type': 'text/plain',
+//          'X-Response-URL': url,
+//        },
+//      })
+//    }
+//
+//    return res
+//  } catch (err) {
+//    console.error(`GET request failed at ${url}`, err)
+//
+//    if (pathShip === window.ship) {
+//      const apiUrl = await findPathUrl(`${pathArray[0]}/api/${endpoint}`)
+//
+//      if (!apiUrl) {
+//        console.error(`No URL found for ${path}`)
+//
+//        return new Response(`No URL found for ${path}`, {
+//          status: 404,
+//          headers: { 'Content-Type': 'text/plain' },
+//        })
+//      }
+//
+//      try {
+//        const res = await fetch(apiUrl, {
+//          method: 'GET',
+//          credentials: 'include',
+//        })
+//
+//        if (!res.ok) {
+//          throw new Error(`Response not ok for ${apiUrl}`)
+//        }
+//
+//        return res
+//      } catch (err) {
+//        console.error(`GET request failed at ${apiUrl}`, err)
+//
+//        return new Response(`File not found for ${path}`, {
+//          status: 404,
+//          headers: {
+//            'Content-Type': 'text/plain',
+//            'X-Response-URL': apiUrl,
+//          },
+//        })
+//      }
+//    }
+//
+//    return new Response(`File not found for ${path}`, {
+//      status: 404,
+//      headers: {
+//        'Content-Type': 'text/plain',
+//        'X-Response-URL': url,
+//      },
+//    })
+//  }
+//}
 
 async function put(path: string, file: File): Promise<Response | void> {
   const pathArray = path.split('/')
