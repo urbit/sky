b8c76afc0669c5959e7f5260c738a3baa7c93f48
bonbud-macryg <oarsman_phlox.0s@icloud.com>
2025-05-01 14:32:16 +0100

    Comment out isValidPath()

--- ui/src/components/PathBar.tsx
+++ ui/src/components/PathBar.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import useWindowStore from '../state/useWindowStore.ts'
-import ob from 'urbit-ob'
+//import ob from 'urbit-ob'
 
 export default function PathBar({
   id,
@@ -18,71 +18,69 @@ export default function PathBar({
     setInputValue(path || '')
   }, [path])
 
-  const isValidPath = (path: string): boolean => {
-    if (path.startsWith('/')) return false
-    if (path.length > 0 && !/^~/.test(path)) return false
-    const azp = path.split('/')[0]
-
-    // validate @p
-    // TODO this doesn't catch everything, should be
-    //      as robust as the dojo is about this and
-    //      should behave the same way
-    if (azp.startsWith('~')) {
-      // star
-      if (azp.length === 7 && !ob.isValidPatp(azp)) return false
-      // planet
-      if (azp.length === 14 && !ob.isValidPatp(azp)) return false
-      // moon
-      if (azp.length === 21 && !ob.isValidPatp(azp)) return false
-      if (azp.length === 28 && !ob.isValidPatp(azp)) return false
-      // comet
-      if (azp.length === 57 && !ob.isValidPatp(azp)) return false
-    }
-
-    // check endpoint is url-safe
-    const end = `/${path.split('/').slice(1).join('/')}`
-    if (!/^[a-zA-Z0-9/_.-]+$/.test(end)) return false
-
-    return true
-  }
+  //const isValidPath = (path: string): boolean => {
+  //  if (path.startsWith('/')) return false
+  //  if (path.length > 0 && !/^~/.test(path)) return false
+  //  const azp = path.split('/')[0]
+  //
+  //  // validate @p
+  //  // TODO this doesn't catch everything, should be
+  //  //      as robust as the dojo is about this and
+  //  //      should behave the same way
+  //  if (azp.startsWith('~')) {
+  //    // star
+  //    if (azp.length === 7 && !ob.isValidPatp(azp)) return false
+  //    // planet
+  //    if (azp.length === 14 && !ob.isValidPatp(azp)) return false
+  //    // moon
+  //    if (azp.length === 21 && !ob.isValidPatp(azp)) return false
+  //    if (azp.length === 28 && !ob.isValidPatp(azp)) return false
+  //    // comet
+  //    if (azp.length === 57 && !ob.isValidPatp(azp)) return false
+  //  }
+  //
+  //  // check endpoint is url-safe
+  //  const end = `/${path.split('/').slice(1).join('/')}`
+  //  if (!/^[a-zA-Z0-9/_.-]+$/.test(end)) return false
+  //
+  //  return true
+  //}
 
   // TODO if first path segment is azimuth point, convert to @p
   function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault()
-    if (isValidPath(inputValue)) {
-      if (pathBarView && pathBarView.includes(id)) {
-        togglePathBarView(id)
-      }
-
-      updateWindowPath(id, inputValue)
+    if (pathBarView && pathBarView.includes(id)) {
+      togglePathBarView(id)
     }
+
+    updateWindowPath(id, inputValue)
   }
 
   function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
     const newValue = e.target.value
 
-    if (isValidPath(newValue)) {
+    //if (isValidPath(newValue)) {
       setInputValue(newValue)
-    } else {
-      const audioContext = new (window.AudioContext || window.AudioContext)()
-      const oscillator = audioContext.createOscillator()
-      const gainNode = audioContext.createGain()
-
-      oscillator.type = 'sine'
-      oscillator.frequency.setValueAtTime(440, audioContext.currentTime)
-
-      oscillator.connect(gainNode)
-      gainNode.connect(audioContext.destination)
-
-      gainNode.gain.setValueAtTime(0.2, audioContext.currentTime)
-      gainNode.gain.exponentialRampToValueAtTime(
-        0.0001,
-        audioContext.currentTime + 0.2
-      )
-
-      oscillator.start()
-      oscillator.stop(audioContext.currentTime + 0.2)
-    }
+    //} else {
+    //  const audioContext = new (window.AudioContext || window.AudioContext)()
+    //  const oscillator = audioContext.createOscillator()
+    //  const gainNode = audioContext.createGain()
+    //
+    //  oscillator.type = 'sine'
+    //  oscillator.frequency.setValueAtTime(440, audioContext.currentTime)
+    //
+    //  oscillator.connect(gainNode)
+    //  gainNode.connect(audioContext.destination)
+    //
+    //  gainNode.gain.setValueAtTime(0.2, audioContext.currentTime)
+    //  gainNode.gain.exponentialRampToValueAtTime(
+    //    0.0001,
+    //    audioContext.currentTime + 0.2
+    //  )
+    //
+    //  oscillator.start()
+    //  oscillator.stop(audioContext.currentTime + 0.2)
+    //}
   }
 
   return (
